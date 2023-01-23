8a4ca9614f5faea60a32839124f0e52fd0d71202
Marcelo Venancio <marcelovenancio100@gmail.com>
2023-01-23 18:19:47 -0300

    Ajustes no detalhe do item

--- utils/custom_functions.py
+++
@@ -1,9 +0,0 @@
-def plural_records(num: str):
-    num = int(num)
-
-    if num == 0:
-        return 'Nenhum registro encontrado'
-    elif num == 1:
-        return f'{num} registro encontrado'
-    else:
-        return f'{num} registros encontrados'
